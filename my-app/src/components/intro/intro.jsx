eeca63b5f6808eb638068646b16739b62b0be35e
samuel lutz <samlutz89@gmail.com>
2022-03-01 19:08:57 -0800

    getting a basic layout

--- my-app/src/components/intro/intro.jsx
+++ my-app/src/components/intro/intro.jsx
@@ -1,7 +1,28 @@
+import "./intro.css";
+
 const Intro = () => { 
     return(
         <div className="i">
-            this is intro
+           <div className="i-left">
+              <div className="i-left-wrapper">
+                  <h2 className="i-intro">Hello, my name is</h2>
+                  <h1 className="i-name">Samuel Lutz</h1>
+                  <div className="i-title">
+                      <div className="i-title-wrapper">
+                          <div className="i-title-item">Web Developer</div>
+                          <div className="i-title-item">Fullstack</div>
+                          <div className="i-title-item">React</div>
+                      </div>
+                  </div>
+                  <div className="i-desc">
+                      I am a full stack web developer Specilizing in React,
+                       javascript, API, and other services. 
+                  </div>
+              </div>
+           </div>
+           <div className="i-right">
+             <img src="" alt="" className="i-img" />
+           </div>
         </div>
     )}
 
