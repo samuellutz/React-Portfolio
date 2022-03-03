d906050cca1fcab4e8a1921a6ed14e93dddaf71b
samuel lutz <samlutz89@gmail.com>
2022-03-03 13:03:02 -0800

    nav working

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Navbar = () => {
-    <div className="container">
-        <div className="pro">
-            <link href="#projects">Projects</link>
+    return (
+        <div className="container">
+            <div className="pro">
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+                <a href="#resume">Resume</a>
+            </div>
         </div>
-    </div>
+    )
 }
-
 export default Navbar;
