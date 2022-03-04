11e14a98e18636f387e60fade24491ed3eb3a405
samuel lutz <samlutz89@gmail.com>
2022-03-03 18:50:19 -0800

    package issues

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,11 +3,11 @@ import "./navbar.css"
 
 const Navbar = () => {
     return (
-        <div className="container">
+        <div class="container-fluid" className="container">
             <div className="pro">
                 <a href="#projects">Projects</a>
                 <a href="#contact">Contact</a>
-                <a href="#resume">Resume</a>
+                <a href="https://docs.google.com/document/d/1P2TOoRFDyVUBwMIzJULB_uNoPfDUYKN-/edit">Resume</a>
             </div>
         </div>
     )
