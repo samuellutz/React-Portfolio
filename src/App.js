069e26d459488d2e86138c6e97c91cb8053c13e2
samuel lutz <samlutz89@gmail.com>
2022-03-03 17:01:07 -0800

    contatcts working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Intro from "./components/intro/intro";
 import Projectlist from "./components/projectlist/projectlist"
 import Navbar from "./components/navbar/navbar"
+import Contact from "./components/contact/contact";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Intro/>
       <Projectlist />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/contact/contact.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import "./contact.css"
+import Phone from '../../img/phone.png';
+import Email from '../../img/email.png';
+import Github from '../../img/github.png';
+import Link from '../../img/linkedin.png';
+
+const Contact = () =>{
+    return(
+        <div className="cont">
+           <div className="cont-bg">
+                <div className="cont-wrap">
+                    <div className="cont-left">
+                        <h1 className="cont-title">Contact</h1>
+                        <div className="cont-info">
+                            <div className="cont-info-item">
+                                <img
+                                    src={Phone}
+                                    alt="phone"
+                                    className="cont-icon"/>
+                                    #408-641-0962
+
+                            </div>
+                            <div className="cont-info-item">
+                                <img
+                                    src={Email}
+                                    alt="email"
+                                    className="cont-icon"/>
+                                    <a href="mailto: Samuellutz88@gmail.com">
+                                     Samuellutz88@gmail.com
+                                     </a>
+                            </div>
+                            <div className="cont-info-item">
+                                <img
+                                    src={Github}
+                                    alt="phone"
+                                    className="cont-icon"/>
+                                    <a href="https://github.com/samuellutz">
+                                        GitHub
+                                    </a>
+                            </div>
+                            <div className="cont-info-item">
+                                <img
+                                    src={Link}
+                                    alt="phone"
+                                    className="cont-icon"/>
+                                    <a href="https://www.linkedin.com/in/samuel-lutz-77138020b/">
+                                        LinkedIn
+                                    </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cont-right">
+                    <form>
+                            <input type="text" placeholder="Name" name="user_name"/>
+                            <input type="text" placeholder="Subject" name="user_Subject"/>
+                            <input type="text" placeholder="Email" name="user_Email"/>
+                            <textarea rows="5" placeholder="message" name="user_message"></textarea>
+                            <button>submit</button>
+                        </form>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
